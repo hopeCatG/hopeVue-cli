aedb362283d197378c9f6b90d5abbd0974b24f72
hopeCatG <2576755215@qq.com>
2019-07-06 21:52:19 +0800

    一丢丢改动！！

--- hopeVue-cli/src/router/index.js
+++ hopeVue-cli/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import index from '../components/index.vue'  //相对路径
 import commentVue from '../components/comment.vue'  
+
 Vue.component('commentVue', commentVue);
 
 Vue.use(Router)
